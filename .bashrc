ac79fd238604d974782dfe7558618f0144707394
mahmoud-eltahawy <gemmy9300@gmail.com>
2023-05-29 03:31:07 +0300

    add structure to packages

--- .bashrc
+++ .bashrc
@@ -1,22 +1,10 @@
-### EXPORT ###
-export EDITOR='emacsclient -nw'
-export VISUAL='emacsclient -nw'
-export HISTCONTROL=ignoreboth:erasedups
-export PAGER='most'
+#
+# ~/.bashrc
+#
 
-PS1='[\u@\h \W]\$ '
 
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
-
-
-if [ -d "$HOME/.bin" ] ;
-  then PATH="$HOME/.bin:$PATH"
-fi
-
-if [ -d "$HOME/.local/bin" ] ;
-  then PATH="$HOME/.local/bin:$PATH"
-fi
 
 #ignore upper and lowercase when TAB completion
 bind "set completion-ignore-case on"
@@ -46,8 +34,9 @@ ex ()
   fi
 }
 
-[[ -f ~/.bashrc-personal ]] && . ~/.bashrc-personal
-
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
 . "$HOME/.cargo/env"
 
 zellij
--- packages.ts
+++ packages.ts
@@ -1,68 +1,137 @@
-const cargo_packages = [
-  "sccache",
-  "exa",
-  "du-dust",
-  "nu",
-  "bat",
-  "zellij",
-  "irust",
-  "ripgrep",
-  "bacon",
-  "cargo-info",
-  "wiki-tui",
-  "trunk",
-  "create-tauri-app",
-  "paru",
-];
+const packages = {
+  programs: {
+    tauri: {
+      pacman: [
+        "webkit2gtk",
+        "base-devel",
+        "curl",
+        "wget",
+        "openssl",
+        "appmenu-gtk-module",
+        "gtk3",
+        "libappindicator-gtk3",
+        "librsvg",
+        "libvips",
+      ],
+      cargo: [
+        "create-tauri-app",
+        "tauri-cli",
+      ],
+    },
+    emacs: {
+      pacman: [
+        "emacs-nativecomp",
+        "fd",
+        "shellcheck",
+        "cmake",
+        "tidy",
+        "stylelint",
+        "jq",
+        "xclip",
+        "maim",
+        "ripgrep",
+      ],
+      npm: [
+        "js-beautify",
+      ],
+    },
+    xmonad: [
+      "xmonad",
+      "xmonad-contrib",
+      "xmobar",
+      "xterm",
+      "dmenu",
+    ],
+  },
+  aur: [
+    "ttf-fira-code",
+    "ttf-arabeyes-fonts",
+    "gnome-alsamixer",
+    "brave",
+  ],
+  get cargo() {
+    return [
+      "sccache",
+      "exa",
+      "du-dust",
+      "nu",
+      "bat",
+      "zellij",
+      "irust",
+      "bacon",
+      "wiki-tui",
+      "cargo-info",
+      "cargo-leptos",
+      "leptosfmt",
+      "trunk",
+      "paru",
+      ...this.programs.tauri.cargo,
+    ];
+  },
+  get pacman() {
+    return [
+      "xorg",
+      "picom",
+      "nitrogen",
+      "alacritty",
+      "btop",
+      "vifm",
+      "mpv",
+      "vlc",
+      "nodejs",
+      "npm",
+      "unzip",
+      ...this.programs.xmonad,
+      ...this.programs.emacs.pacman,
+      ...this.programs.tauri.pacman,
+    ];
+  },
+  get npm() {
+    return [
+      ...this.programs.emacs.npm,
+    ];
+  },
+  install: async function () {
+    await Deno.run({
+      cmd: ["sudo", "pacman", "-S", "--needed", ...this.pacman],
+    }).status();
+    await Deno.run({ cmd: ["cargo", "install", ...this.cargo] }).status();
+    await Deno.run({ cmd: ["paru", "-S", ...this.aur] }).status();
+    await Deno.run({ cmd: ["sudo", "npm", "install", "-g", ...this.npm] })
+      .status();
+  },
+};
 
-const pacman_packages = [
-  "xorg",
-  "emacs-nativecomp",
-  "picom",
-  "nitrogen",
-  "dmenu",
-  "alacritty",
-  "xterm",
-  "fish",
-  "btop",
-  "vifm",
-  "mpv",
-  "vlc",
-  "fd",
-  "shellcheck",
-  "cmake",
-  "tidy",
-  "stylelint",
-  "jq",
-  "xclip",
-  "maim",
-  "xmonad",
-  "xmonad-contrib",
-  "xmobar",
-  "nodejs",
-  "npm",
-];
+const rust = {
+  targets: [
+    "wasm32-unknown-unknown",
+  ],
+  components: [
+    "rust-analyzer",
+  ],
+  install: async function () {
+    const s = Deno.run({
+      cmd: ["curl", "https://sh.rustup.rs"],
+      stdout: "piped",
+    });
+    await s.status();
+    const script = new TextDecoder().decode(await s.output());
+    const file_path = "./tmp_install_script";
+    await Deno.create(file_path);
+    await Deno.writeTextFile(file_path, script);
+    await Deno.run({ cmd: ["sh", file_path] }).status();
+    await Deno.remove(file_path);
+    await Deno.run({ cmd: ["source", "'$HOME/.cargo/env'"] }).status();
+    await Deno.run({ cmd: ["rustup", "target", "add", ...this.targets] })
+      .status();
+    await Deno.run({ cmd: ["rustup", "component", "add", ...this.components] })
+      .status();
+  },
+};
 
-const aur_packages = [
-  "otf-fira-code-git"
-];
-
-await install_rust();
-await Deno.run({ cmd: ["cargo", "install", ...cargo_packages] }).status();
-await Deno.run({ cmd: ["sudo", "pacman", "-S", ...pacman_packages] }).status();
-await Deno.run({ cmd: ["paru", "-S", ...aur_packages] }).status();
+await rust.install();
+await packages.install();
 await install_doom_emacs();
-
-async function install_rust() {
-  const s = Deno.run({ cmd: ["curl", "https://sh.rustup.rs"], stdout: "piped" });
-  await s.status();
-  const script = new TextDecoder().decode(await s.output());
-  const file_path = "./tmp_install_script";
-  await Deno.create(file_path);
-  await Deno.writeTextFile(file_path, script);
-  await Deno.run({ cmd: ["sh", file_path] }).status();
-  await Deno.remove(file_path);
-}
 
 async function install_doom_emacs() {
   const emacsHome = await Deno.env.get("HOME") + "/.config/emacs";
