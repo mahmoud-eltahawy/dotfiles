d976e34369526b578768cd283ad306753b69f9df
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-10-13 13:42:17 +0300

    move zoom to haskell

---
+++ zoom/Main.hs
@@ -0,0 +1,30 @@
+import System.Process
+import Data.String
+import Prelude hiding (lines)
+import System.Environment (getArgs)
+import Data.Maybe (listToMaybe)
+
+zoom :: Float -> IO String
+zoom factor = readProcess "hyprctl" ["keyword", "cursor:zoom_factor", show factor] ""
+
+getCurrent :: IO Float
+getCurrent = read.dropWhile (/= ' ').head.lines <$> readProcess "hyprctl" ["getoption", "cursor:zoom_factor"] ""
+
+actCurrent :: String -> Float -> IO String
+actCurrent "in" current = zoom $ current + 0.5
+actCurrent "out" current = zoom $ current - 0.5
+actCurrent _ current = zoom 1.0 
+
+act :: String -> IO String
+act arg = do
+    current <- getCurrent
+    actCurrent arg current
+
+
+main :: IO()
+main = do
+    arg <- listToMaybe <$> getArgs
+    _ <- case arg of 
+        Just arg -> act arg
+        Nothing -> act ""
+    print "Ok"
